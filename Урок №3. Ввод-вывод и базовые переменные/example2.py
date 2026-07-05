6aecd090e0600380c5d96366fb3f6f6451a6c3e9
aR34temGENuis <theuselessIdeas@icloud.com>
2026-07-05 12:34:53 +0300

    Урок №3. Ввод-вывод и базовые переменные.

---
+++ Урок №3. Ввод-вывод и базовые переменные/example2.py
@@ -0,0 +1,13 @@
+# https://ru.wikipedia.org/wiki/%D0%A5%D1%80%D0%BE%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F_%D1%8D%D0%B2%D0%BE%D0%BB%D1%8E%D1%86%D0%B8%D0%B8_%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0
+stage1 = input("Введите 1 стадию развития человека: ") # Homo habilis
+stage2 = input("Введите 2 стадию развития человека: ") # Homo rudolfensis
+stage3 = input("Введите 3 стадию развития человека: ") # Homo erectus
+stage4 = input("Введите 4 стадию развития человека: ") # Homo georgicus
+stage5 = input("Введите 5 стадию развития человека: ") # Homo ergaster
+stage6 = input("Введите 6 стадию развития человека: ") # Homo antecessor
+stage7 = input("Введите 7 стадию развития человека: ") # Homo cepranensis
+stage8 = input("Введите 8 стадию развития человека: ") # Homo heidelbergensis
+stage9 = input("Введите 9 стадию развития человека: ") # Homo rhodesiensis
+stage10 = input("Введите 10 стадию развития человека: ") # Homo neanderthalensis
+stage11 = input("Введите 11 стадию развития человека: ") # Homo sapiens sapiens
+print(stage1, stage2, stage3, stage4, stage5, stage6, stage7, stage8, stage9, stage10, stage11, sep="=>")
